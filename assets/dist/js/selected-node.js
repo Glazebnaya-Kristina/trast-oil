de8ec5d78c70d074fb4a68742e5662890065f5aa
Glazebnaya-Kristina <46931114+Glazebnaya-Kristina@users.noreply.github.com>
2019-12-10 13:09:02 +0200

    Разметка секции recommendations. Добавлены остальные секции с друих страниц. Измене цвет для __text в любых блоках.

--- assets/dist/js/selected-node.js
+++ assets/dist/js/selected-node.js
@@ -1,3 +1,43 @@
 $(document).ready(function () {
+   $('.recommendations__carousel').slick({
+      slidesToShow: 3,
+      dots: false,
+      focusOnSelect: true,
+      infinite: true,
 
+      prevArrow: '<button type="button" class="button-arrow button-arrow--left recommendations__arrow recommendations__arrow--left"><i class="icon-arrow"></i></button>',
+      nextArrow: '<button type="button" class="button-arrow recommendations__arrow recommendations__arrow--right"><i class="icon-arrow"></i></button>',
+      responsive: [
+         {
+            breakpoint: 1434,
+            settings: {
+               slidesToShow: 2,
+               arrows: true
+            }
+         },
+         {
+            breakpoint: 1023,
+            settings: {
+               slidesToShow: 2,
+               arrows: false
+            }
+         },
+         {
+            breakpoint: 768,
+            settings: {
+               slidesToShow: 1,
+               arrows: false
+            }
+         }
+      ]
+   });
+
+   $('.reviews__carousel').slick({
+      slidesToShow: 1,
+      dots: false,
+      infinite: true,
+      fade: true,
+      prevArrow: $('.reviews__arrow--left'),
+      nextArrow: $('.reviews__arrow--right')
+   });
 });
