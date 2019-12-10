6c7c2da5e2fdbe3e05a20b0a7728b974e1542915
Glazebnaya-Kristina <46931114+Glazebnaya-Kristina@users.noreply.github.com>
2019-12-10 08:43:14 +0200

    Разметка секции main-banner.

--- assets/dist/js/home.js
+++ assets/dist/js/home.js
@@ -12,6 +12,8 @@ $(document).ready(function () {
    $('.certificates__carousel').slick({
       slidesToShow: 4,
       dots: false,
+      // centerMode: true,
+      focusOnSelect: true,
       infinite: true,
       prevArrow: '<button type="button" class="button-arrow button-arrow--left certificates__arrow certificates__arrow--left"><i class="icon-arrow"></i></button>',
       nextArrow: '<button type="button" class="button-arrow certificates__arrow certificates__arrow--right"><i class="icon-arrow"></i></button>',
@@ -31,6 +33,20 @@ $(document).ready(function () {
       ]
    });
 
+   // $('.main-banner__carousel').slick({
+   //    slidesToShow: 3,
+   //    focusOnSelect: true,
+   //    dots: false,
+   //    centerMode: true,
+   //    centerPadding: '0',
+   //    infinite: true,
+   //    arrows: false,
+   //    variableWidth: true
+   //    // fade: true,
+   //    // prevArrow: $('.reviews__arrow--left'),
+   //    // nextArrow: $('.reviews__arrow--right')
+   // });
+
    // $('.certificates__item .slick-slide .slick-active:nth-of-type(4)').css('transform', 'scale(1.2)');
    // $('.certificates__carousel').find('.slick-active').click(function () {
    //    // var indexItem = $(this).index();
@@ -44,9 +60,9 @@ $(document).ready(function () {
    // });
 
 
-   $('.certificates__carousel .certificates__item').each(function (index) {
-      $(this).find('.slick-active:eq(-2)').css('transform', 'scale(1.2)');
-   });
+   // $('.certificates__carousel .certificates__item').each(function (index) {
+   //    $(this).find('.slick-active:eq(-2)').css('transform', 'scale(1.2)');
+   // });
 
 
    /* курсив для текста второго <li> в каждом <ul class="nav"> */
@@ -63,6 +79,37 @@ $(document).ready(function () {
    //    }
    // })
 
+
+   $('.main-banner__carousel').slick({
+      infinite: true,
+      slidesToShow: 3,
+      arrows: false,
+      focusOnSelect: true,
+      centerMode: true,
+      centerPadding: '0',
+      variableWidth: true,
+      dots: true,
+      appendDots:$(".main-banner__dots"),
+      dotsClass: 'main-banner__dots',
+      customPaging: function (slider, i) {
+         var current = i + 1;
+         current = current < 10 ? "0" + current : current;
+         var thumb = $(slider.$slides[current]).data();
+         return '<button type="button" class="main-banner__dot-item">' + current + '</button>';
+      },
+      responsive: [
+         {
+            breakpoint: 768,
+            settings: {
+               slidesToShow: 1,
+               slidesToScroll: 1,
+               arrows: false,
+               centerMode: false,
+               variableWidth: false
+            }
+         }
+      ]
+   });
 });
 
 
