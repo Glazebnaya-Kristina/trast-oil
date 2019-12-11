95ab1dd07fd9468008c760ec2abf93c29cad166f
Glazebnaya-Kristina <46931114+Glazebnaya-Kristina@users.noreply.github.com>
2019-12-11 08:49:01 +0200

    Добавлена якорная кнопка наверх.

--- assets/dist/js/field-selection.js
+++ assets/dist/js/field-selection.js
@@ -7,5 +7,22 @@ $(document).ready(function () {
       $('.accordeon .accordeon__head').not(this).removeClass('accordeon__head--active').next().slideUp();
    });
 
+   var $anchor = $( '.js-anchor' );
+
+   if ( $anchor.length ) {
+      $anchor.on('click', function(event) {
+
+         event.preventDefault();
+         var $that = $( this ), hb = $('body, html');
+
+         if ( $that.is( 'button' ) ) {
+            hb.stop().animate({ scrollTop: $( '.' + $that.data( 'id' ) ).offset().top }, 1000, 'swing');
+         } else if ( $that.is( 'a' ) ) {
+            hb.stop().animate({ scrollTop: $( '.' + $that.attr( 'href' ).replace('#', '') ).offset().top }, 1000, 'swing');
+         }
+
+      });
+   }
+
 
 });
