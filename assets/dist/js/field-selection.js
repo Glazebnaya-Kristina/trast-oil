0ec524dc96234c47d47677d37e57bd4938e1ae53
Glazebnaya-Kristina <46931114+Glazebnaya-Kristina@users.noreply.github.com>
2019-12-11 07:59:29 +0200

    Разметка блока accordeon в секции field-selection

--- assets/dist/js/field-selection.js
+++ assets/dist/js/field-selection.js
@@ -2,4 +2,10 @@ $(document).ready(function () {
 
    jcf.replaceAll();
 
+   $('.accordeon .accordeon__head').click(function () {
+      $(this).toggleClass('accordeon__head--active').next().slideToggle();
+      $('.accordeon .accordeon__head').not(this).removeClass('accordeon__head--active').next().slideUp();
+   });
+
+
 });
