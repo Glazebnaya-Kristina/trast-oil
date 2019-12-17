3dec09a1348ddb889f80203b78e557e9a1d38542
Glazebnaya-Kristina <46931114+Glazebnaya-Kristina@users.noreply.github.com>
2019-12-17 15:04:57 +0200

    Написан код для слик шапки под разное разрешения.

--- assets/dist/js/script.js
+++ assets/dist/js/script.js
@@ -1,50 +1,91 @@
 $(document).ready(function () {
-   //бергр меню
-   // $('.menu-burger').on('click', function(e){
-   //    e.preventDefault();
-   //
-   //    $('.menu-burger').toggleClass('menu-burger--open');
-   //    $('.navigation').toggleClass('navigation--opened');
-   //    $('body').toggleClass('hidden')
-   // });
-
    //якорные ссылки
-   var $anchor = $( '.js-anchor' );
+   var $anchor = $('.js-anchor');
 
-   if ( $anchor.length ) {
-      $anchor.on('click', function(event) {
+   if ($anchor.length) {
+      $anchor.on('click', function (event) {
 
          event.preventDefault();
-         var $that = $( this ), hb = $('body, html');
+         var $that = $(this), hb = $('body, html');
 
-         if ( $that.is( 'button' ) ) {
-            hb.stop().animate({ scrollTop: $( '.' + $that.data( 'id' ) ).offset().top }, 1000, 'swing');
-         } else if ( $that.is( 'a' ) ) {
-            hb.stop().animate({ scrollTop: $( '.' + $that.attr( 'href' ).replace('#', '') ).offset().top }, 1000, 'swing');
+         if ($that.is('button')) {
+            hb.stop().animate({scrollTop: $('.' + $that.data('id')).offset().top}, 1000, 'swing');
+         } else if ($that.is('a')) {
+            hb.stop().animate({scrollTop: $('.' + $that.attr('href').replace('#', '')).offset().top}, 1000, 'swing');
          }
 
       });
    }
+
+   // слик шапка, для разных разрешениях
+   function toggleHeader() {
+      var scroll_status = $(document).scrollTop();
+      if (scroll_status > 0)
+         $(".header").addClass("header--fixed");
+      else
+         $(".header").removeClass("header--fixed");
+   }
+
+   function toggleHeader2() {
+      var scroll_status = $(document).scrollTop();
+      if (scroll_status > 0)
+         $(".header").addClass("header--fixed-mob");
+      else
+         $(".header").removeClass("header--fixed-mob");
+   }
+
+   function toggleHeader3() {
+      var scroll_status = $(document).scrollTop();
+      if (scroll_status > 0)
+         $(".header").addClass("header--fixed-tabl");
+      else
+         $(".header").removeClass("header--fixed-tabl");
+   }
+
+
+   $(document).scroll(function () {
+      width = $(window).width();
+      if (width >= 1429) {
+         toggleHeader();
+
+      } else if(width >= 768) {
+         toggleHeader3();
+      } else {
+         toggleHeader2();
+      }
+   });
+
 });
 
-!function(e){"function"!=typeof e.matches&&(e.matches=e.msMatchesSelector||e.mozMatchesSelector||e.webkitMatchesSelector||function(e){for(var t=this,o=(t.document||t.ownerDocument).querySelectorAll(e),n=0;o[n]&&o[n]!==t;)++n;return Boolean(o[n])}),"function"!=typeof e.closest&&(e.closest=function(e){for(var t=this;t&&1===t.nodeType;){if(t.matches(e))return t;t=t.parentNode}return null})}(window.Element.prototype);
+!function (e) {
+   "function" != typeof e.matches && (e.matches = e.msMatchesSelector || e.mozMatchesSelector || e.webkitMatchesSelector || function (e) {
+      for (var t = this, o = (t.document || t.ownerDocument).querySelectorAll(e), n = 0; o[n] && o[n] !== t;) ++n;
+      return Boolean(o[n])
+   }), "function" != typeof e.closest && (e.closest = function (e) {
+      for (var t = this; t && 1 === t.nodeType;) {
+         if (t.matches(e)) return t;
+         t = t.parentNode
+      }
+      return null
+   })
+}(window.Element.prototype);
 
 // модальные окна
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
 
    /* Записываем в переменные массив элементов-кнопок и подложку.
       Подложке зададим id, чтобы не влиять на другие элементы с классом overlay*/
    var modalButtons = document.querySelectorAll('.js-open-modal'),
-      overlay      = document.querySelector('.js-overlay-modal'),
+      overlay = document.querySelector('.js-overlay-modal'),
       closeButtons = document.querySelectorAll('.close');
-      modalWrapp = document.querySelector('.modal');
+   modalWrapp = document.querySelector('.modal');
 
 
    /* Перебираем массив кнопок */
-   modalButtons.forEach(function(item){
+   modalButtons.forEach(function (item) {
 
       /* Назначаем каждой кнопке обработчик клика */
-      item.addEventListener('click', function(e) {
+      item.addEventListener('click', function (e) {
 
          /* Предотвращаем стандартное действие элемента. Так как кнопку разные
             люди могут сделать по-разному. Кто-то сделает ссылку, кто-то кнопку.
@@ -68,9 +109,9 @@ document.addEventListener('DOMContentLoaded', function() {
 
    }); // end foreach
 
-   closeButtons.forEach(function(item){
+   closeButtons.forEach(function (item) {
 
-      item.addEventListener('click', function(e) {
+      item.addEventListener('click', function (e) {
          var parentModal = this.closest('.popup');
 
          parentModal.classList.remove('popup--opened');
@@ -91,11 +132,12 @@ document.addEventListener('DOMContentLoaded', function() {
          document.querySelector('.modal__overlay--opened').classList.remove('modal__overlay--opened');
          document.querySelector('.popup--opened').classList.remove('popup--opened');
          document.querySelector('.hidden').classList.remove('hidden');
-      };
+      }
+      ;
    }, false);
 
 
-   overlay.addEventListener('click', function() {
+   overlay.addEventListener('click', function () {
       document.querySelector('.modal--opened').classList.remove('modal--opened');
       document.querySelector('.popup--opened').classList.remove('popup--opened');
       document.querySelector('.hidden').classList.remove('hidden');
